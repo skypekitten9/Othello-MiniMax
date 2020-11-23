84c3e0a337723504db146b4a5bf7e87814874be8
skypekitten9 <victor.enhorning@gmail.com>
2020-11-23 12:05:34 +0100

    Designed tile and pawn, added a script to turn between tile-states.

---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum TileState { Black, White, Empty };
+public class TileScript : MonoBehaviour
+{
+    TileState tileState;
+    Transform pawn;
+
+    void Start()
+    {
+        pawn = gameObject.GetComponentInChildren<Transform>().Find("Pawn").transform;
+        TurnTile(TileState.Empty);
+    }
+
+    void TurnTile(TileState stateToTurn)
+    {
+        pawn.gameObject.SetActive(true);
+        switch (stateToTurn)
+        {
+            case TileState.Black:
+                pawn.rotation = new Quaternion(180, 0, 0, 1);
+                break;
+            case TileState.White:
+                pawn.rotation = Quaternion.identity;
+                break;
+            case TileState.Empty:
+                pawn.gameObject.SetActive(false);
+                break;
+            default:
+                break;
+        }
+        tileState = stateToTurn;
+    }
+
+    IEnumerator TestStates()
+    {
+        TurnTile(TileState.Black);
+        yield return new WaitForSeconds(1f);
+        TurnTile(TileState.White);
+        yield return new WaitForSeconds(1f);
+        TurnTile(TileState.Empty);
+        yield return new WaitForSeconds(1f);
+        TurnTile(TileState.White);
+        yield return new WaitForSeconds(1f);
+    }
+}
