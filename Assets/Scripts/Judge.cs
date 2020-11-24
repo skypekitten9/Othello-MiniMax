ee1a1a0742773b251f3480fe9f47da85afa1e16f
skypekitten9 <victor.enhorning@gmail.com>
2020-11-24 09:42:19 +0100

    Added judge and implemented a basic check to see if a tile is playable.

---
+++ Assets/Scripts/Judge.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Judge
+{
+    public static bool IsTilePlayable(TileState[,] tileStates, int z, int x, TileState turnTo)
+    {
+        if(tileStates[z, x] == TileState.Empty)
+        {
+            return true;
+        }
+        return false;
+    }
+}
