22823b070196ca73b8e1a80274b3c37987f5ed57
skypekitten9 <victor.enhorning@gmail.com>
2021-01-20 12:45:19 +0100

    Smaller fixes for web build.

--- Assets/Scripts/UI_Script.cs
+++ Assets/Scripts/UI_Script.cs
@@ -80,12 +80,13 @@ public class UI_Script : MonoBehaviour
 
     public void SetRefferences(Vector3 topWorldPosition, Vector3 bottomWorldPosition, Vector3 leftWorldPosition, Vector3 rightWorldPosition)
     {
-        gameObject.transform.Find("BlacksTurn").transform.position = Camera.main.WorldToScreenPoint(topWorldPosition);
-        gameObject.transform.Find("WhitesTurn").transform.position = Camera.main.WorldToScreenPoint(topWorldPosition);
+        //gameObject.transform.Find("BlacksTurn").transform.position = GameObject.Find("Main Camera").GetComponent<Camera>().WorldToScreenPoint(topWorldPosition);
+        //gameObject.transform.Find("BlacksTurn").transform.position = Camera.main.WorldToScreenPoint(topWorldPosition);
+        //gameObject.transform.Find("WhitesTurn").transform.position = Camera.main.WorldToScreenPoint(topWorldPosition);
 
-        gameObject.transform.Find("PlayerVsAgent").transform.position = Camera.main.WorldToScreenPoint(bottomWorldPosition);
-        gameObject.transform.Find("PlayerVsPlayer").transform.position = Camera.main.WorldToScreenPoint(leftWorldPosition);
-        gameObject.transform.Find("AgentVsAgent").transform.position = Camera.main.WorldToScreenPoint(rightWorldPosition);
+        //gameObject.transform.Find("PlayerVsAgent").transform.position = Camera.main.WorldToScreenPoint(bottomWorldPosition);
+        //gameObject.transform.Find("PlayerVsPlayer").transform.position = Camera.main.WorldToScreenPoint(leftWorldPosition);
+        //gameObject.transform.Find("AgentVsAgent").transform.position = Camera.main.WorldToScreenPoint(rightWorldPosition);
     }
 
     public void ExitGame()
